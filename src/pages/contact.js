70661945b7a6a9487a6835f808299e4b7b1d9c0d
Manujapammy <manujats1998@gmail.com>
2026-04-03 17:05:48 +0530

    added mallavalli office contacts and updated logo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,86 +19,73 @@ export default function Contact() {
                                         <span>Near Hosahalli Circle, Mandya-571401</span></span></span>
                             </div>
                             <div class="col">
-                                <h4 class="contactAddressH4">Bangalore Office</h4>
+                                <h4 class="contactAddressH4">Malavalli Office</h4>
                                 <span className="contactAddressSpan"><span>P2 Construction,<br></br> </span>
-                                    <span> 401/A, 80 feet road, <span> Hoysala Circle,  </span>
-                                        <span>K S town, Bangalore-560060</span></span></span>
+                                    <span> Opposite to kalamma temple, <span> Near KSRTC bus stand,</span>
+                                        <span>Mallavalli tq, Mandya-571430</span></span></span>
                             </div>
                         </div>
-                       
+
                         <div>
-                            <h1 class="heading"> STAY
-                                <span> CONNECTED </span></h1>
-                            <div class="Home_contactinfos__3WQGA">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="Home_contactdtcover__HCNHX">
-                                            <div class="Home_leftctinfo__1V-0_">
-                                                <div class="contactUsDiv1">
+                            <h1 className="heading">
+                                STAY <span>CONNECTED</span>
+                            </h1>
+
+                            <div className="Home_contactinfos__3WQGA">
+                                <div className="row text-center">
+
+                                    {/* Mail section */}
+                                    <div className="col-md-6">
+                                        <div className="Home_contactdtcover__HCNHX">
+                                            <div className="Home_leftctinfo__1V-0_">
+                                                <div className="contactUsDiv1">
                                                     <div style={{ boxSizing: "border-box", display: "block", maxWidth: "100%" }}>
-                                                        <img alt="" aria-hidden="true" role="presentation"
+                                                        <img
+                                                            alt="Email icon"
                                                             src="/images/Gmailimage.webp"
-                                                            class="contactImageStyle"
+                                                            className="contactImageStyle"
                                                         />
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="Home_rightctinfo__2nWLG">
-                                                <h4>Mail us at</h4>
+                                            <div className="Home_rightctinfo__2nWLG">
+                                                <h4>Mail US</h4>
                                                 <p>
-                                                    <a href="mailto:p2constructionmandya@gmail.com" 
-                                                    style={{fontSize:"larger"}}>p2constructionmandya@gmail.com</a>
+                                                    <a href="mailto:p2constructionmandya@gmail.com" style={{ fontSize: "larger" }}>
+                                                        p2constructionmandya@gmail.com
+                                                    </a>
                                                 </p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col">
-                                        <div class="Home_contactdtcover__HCNHX">
-                                            <div class="Home_leftctinfo__1V-0_">
-                                                <div class="contactUsDiv1">
+
+                                    {/* Contact section */}
+                                    <div className="col-md-6">
+                                        <div className="Home_contactdtcover__HCNHX">
+                                            <div className="Home_leftctinfo__1V-0_">
+                                                <div className="contactUsDiv1">
                                                     <div style={{ boxSizing: "border-box", display: "block", maxWidth: "100%" }}>
-                                                        <img alt="" aria-hidden="true"
-                                                            role="presentation"
+                                                        <img
+                                                            alt="Phone icon"
                                                             src="/images/callUsimage.webp"
-                                                            class="contactImageStyle"
+                                                            className="contactImageStyle"
                                                         />
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="Home_rightctinfo__2nWLG">
+                                            <div className="Home_rightctinfo__2nWLG">
                                                 <h4>Contact Us</h4>
-                                                <p><a href="tel:+919742131171">(91) 8073096564</a>
+                                                <p>
+                                                    <a href="tel:+9198073096564">(91) 8073096564</a>
                                                 </p>
                                             </div>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
-                        {/* <div className="contact-information">
-                            <ul>
-                                <li>
-                                    <span className="icon" >
-                                        <LocationMarkerIcon style={{ height: "40%", width: "-webkit - fill - available" }} />
-                                    </span>
-                                    <h3 className="text text-light"> Address</h3>
-                                     <span className="text"><span>P2 Construction , </span>
-                                        <span> Stock Building, Hosahalli, Mandya, Karnataka, India</span></span> 
-                                </li>
-                                <li>
-                                    <span className="icon">
-                                        <PhoneIcon />
-                                    </span>
-                                    <span className="text"><a href="tel:9742131171">(91) 9742131171</a><a href="tel:8073096564">(91) 8073096564</a></span>
-                                </li>
-                                <li>
-                                    <span className="icon">
-                                        <MailIcon />
-                                    </span>
-                                    <span className="text"><a href="mailto:p2constructionmandya@gmail.com">p2constructionmandya@gmail.com</a><a href="mailto:info@example.com">info@example.com</a></span>
-                                </li>
-                            </ul>
-                        </div> */}
+                        
                     </div>
                     <div class="col paddingFree" >
                         <form>
